c0f659f54563a6dec3d6d7429753d5ef1e18f812
andrea rota <a@xelera.eu>
2013-02-13 20:26:37 +0000

    fixed typo

--- legambiente/templates/deprecate-internet-explorer.php
+++ legambiente/templates/deprecate-internet-explorer.php
@@ -1,7 +1,7 @@
 
 <!--[if lt IE 9 ]>
   <p class='flash top chromeframe'>
-    State utilizzando un browser internet obsolete.
+    State utilizzando un browser internet obsoleto.
     <a href="http://browsehappy.com/">Aggiornatelo</a> o
     <a href="http://www.google.com/chromeframe/?redirect=true">installate Google Chrome Frame</a>
     per visualizzare questo sito correttamente.

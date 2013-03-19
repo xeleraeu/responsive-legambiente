bb7b428ab8e2a9651f0e39d9b0707f72521c5c46
andrea rota <a@xelera.eu>
2013-03-19 19:28:38 +0000

    how do i get the id

--- legambiente/includes/functions.php
+++ legambiente/includes/functions.php
@@ -49,8 +49,8 @@ add_action('responsive_container', 'deprecate_internet_explorer');
 if(!function_exists('legambiente_featured_video')) {
   function legambiente_featured_video() {
     $post_type = get_post_type();
-    error_log($post->ID);
-    $featured_video_id = get_post_meta($post->ID, 'featured_video', true);
+    error_log(get_the_ID());
+    $featured_video_id = get_post_meta(get_the_ID(), 'featured_video', true);
     if(($post_type === 'page' or $post_type === 'post') and $featured_video_id) {
       
       $featured_video = pods('video', $featured_video_id, true);

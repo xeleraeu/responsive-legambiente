be4e0b5fdeff8f1bef59e54aaea1c9f5f1fc6201
andrea rota <a@xelera.eu>
2013-03-19 21:34:57 +0000

    support for youtube video via mediaelements

--- legambiente/templates/featured-video.php
+++ legambiente/templates/featured-video.php
@@ -1,3 +1,9 @@
 <?php if($featured_video->exists()): ?>
-<?php echo $featured_video->field('name'); ?>
+<div id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>">
+  <video id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>-player" preload="none">
+  <?php if($featured_video->field('youtube_id')): ?>
+    <source type="video/youtube" src="http://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />
+  <?php endif; // ($featured_video->field('youtube_id')) ?>
+  </video>
+</div>
 <?php endif; // ($featured_video->exists()) ?>

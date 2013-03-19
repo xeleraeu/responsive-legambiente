d55590a81cb0ada84a2f03a113467da7029bc650
andrea rota <a@xelera.eu>
2013-03-19 22:09:42 +0000

    run mediaelements

--- legambiente/templates/featured-video.php
+++ legambiente/templates/featured-video.php
@@ -3,7 +3,14 @@
   <video id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>-player" preload="none">
   <?php if($featured_video->field('youtube_id')): ?>
     <source type="video/youtube" src="http://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />
+  <?php elseif($featured_video->field('video_file_uri'): ?>
+    <source type="video/webm" src="<?php echo $featured_video->field('video_file_uri'); ?>" />
   <?php endif; // ($featured_video->field('youtube_id')) ?>
   </video>
 </div>
+<script>
+jQuery(document).ready(function($) {
+    $('#legambiente-featured-video-<?php echo $featured_video->field('id'); ?>-player').mediaelementplayer();
+});
+</script>
 <?php endif; // ($featured_video->exists()) ?>

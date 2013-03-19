7ba198ffe34e183b14278658ed390b45882ad8ba
andrea rota <a@xelera.eu>
2013-03-19 22:18:05 +0000

    set video width

--- legambiente/templates/featured-video.php
+++ legambiente/templates/featured-video.php
@@ -1,5 +1,5 @@
 <?php if($featured_video->exists()): ?>
-<div id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>">
+<div id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>" width="100%">
   <video id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>-player" preload="none">
   <?php if($featured_video->field('youtube_id')): ?>
     <source type="video/youtube" src="http://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />

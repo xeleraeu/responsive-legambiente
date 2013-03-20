e3f120b3a6bbd73cb380fbd2e9aa6383d94ab160
andrea rota <a@xelera.eu>
2013-03-20 09:36:52 +0000

    added support for vimeo

--- legambiente/templates/featured-video.php
+++ legambiente/templates/featured-video.php
@@ -2,7 +2,9 @@
 <div id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>" class="pods-video">
   <video id="legambiente-featured-video-<?php echo $featured_video->field('id'); ?>-player" preload="none" width="320" height="180" style="width: 100%; height: 100%; max-width: 100%;">
   <?php if($featured_video->field('youtube_id')): ?>
-    <source type="video/youtube" src="http://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />
+    <source type="video/x-youtube" src="https://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />
+  <?php elseif($featured_video->field('vimeo_id')): ?>
+    <source type="video/x-vimeo" src="https://www.vimeo.com/<?php echo $featured_video->field('vimeo_id'); ?>" />
   <?php elseif($featured_video->field('video_file_uri')): ?>
     <source type="video/webm" src="<?php echo $featured_video->field('video_file_uri'); ?>" />
   <?php endif; // ($featured_video->field('youtube_id')) ?>

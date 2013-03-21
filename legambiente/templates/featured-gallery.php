4bb53de0b150c03f8e049a0001faa6fa561033ef
andrea rota <a@xelera.eu>
2013-03-21 00:14:34 +0000

    cannot do that in PHP sorry?

--- legambiente/templates/featured-gallery.php
+++ legambiente/templates/featured-gallery.php
@@ -1,7 +1,8 @@
 <?php if($featured_gallery->exists()): ?>
 <div id="legambiente-featured-gallery-<?php echo $featured_gallery->field('id'); ?>" class="pods-photo-gallery">
   <?php
-    if($photos = $featured_gallery->field('photo')):
+    if($featured_gallery->field('photo')):
+      $photos = $featured_gallery->field('photo');
       foreach($photos as $photo):
   ?>
   <img src="<?php echo wp_get_attachment_url($photo->ID); ?>" />

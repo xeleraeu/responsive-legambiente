fc72dc61b56e7db4c5c97fe38cb8786b220450b2
andrea rota <a@xelera.eu>
2013-04-05 20:09:07 +0100

    added title

--- legambiente/templates/post-collection-sidebar-widget.php
+++ legambiente/templates/post-collection-sidebar-widget.php
@@ -3,19 +3,20 @@
   if(count($slider_posts)):
 ?>
   <div class="widget-wrapper widget_la_post_collection">
-      <ul class="items">
-        <?php
-        foreach($slider_posts as $post): setup_postdata($post);
-        ?>
-        <li>
-          <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-          </a>
-        </li>
-        <?php
-        endforeach;
-        ?>
-      </ul>
+    <div class="widget-title">Articoli in evidenza</div>
+    <ul class="items">
+      <?php
+      foreach($slider_posts as $post): setup_postdata($post);
+      ?>
+      <li>
+        <a href="<?php the_permalink(); ?>">
+          <?php the_title(); ?>
+        </a>
+      </li>
+      <?php
+      endforeach;
+      ?>
+    </ul>
   </div>
 <?php
   endif; // (count($slider_posts))

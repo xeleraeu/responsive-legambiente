4f9bfc2e91fc060523afd7a1466c1ea650ba3b06
andrea rota <a@xelera.eu>
2013-04-05 20:30:26 +0100

    fixed syntax

--- legambiente/includes/functions.php
+++ legambiente/includes/functions.php
@@ -210,7 +210,7 @@ if(!function_exists('legambiente_do_featured_collection')) {
     if($collection_data['widget_type'] === 'sidebar' and count($slider_posts)) {
       locate_template('templates/post-collection-sidebar-widget.php', true, false);
     } elseif($collection_data['widget_type'] === 'slider' and count($slider_posts)) {
-      locate_template('templates/post-collection.php', true, true);
+      locate_template('templates/post-collection.php', true, false);
     }
     $post = $original_post;
   }

967813fe5f5c0fc463792315ac29e2b24496a9a6
andrea rota <a@xelera.eu>
2013-04-08 11:23:56 +0100

    replaced mediaelementjs with upstream embed codes for youtube and vimeo

--- legambiente/templates/featured-video.php
+++ legambiente/templates/featured-video.php
@@ -1,13 +1,13 @@
 <?php if($featured_video->exists()): ?>
 <div id="<?php echo $featured_video_uniqid; ?>" class="pods-video">
-  <video id="<?php echo $featured_video_uniqid; ?>-player" preload="none" width="320" height="180" style="width: 100%; height: 100%; max-width: 100%;">
   <?php if($featured_video->field('youtube_id')): ?>
-    <source type="video/youtube" src="https://www.youtube.com/watch?v=<?php echo $featured_video->field('youtube_id'); ?>" />
+    <iframe type="text/html" width="100%" height="auto" src="http://www.youtube.com/embed/<?php echo $featured_video->field('youtube_id'); ?>?autoplay=0&origin=http://example.com" frameborder="0"/>
   <?php elseif($featured_video->field('vimeo_id')): ?>
-    <source type="video/vimeo" src="https://www.vimeo.com/<?php echo $featured_video->field('vimeo_id'); ?>" />
+    <iframe src="http://player.vimeo.com/video/<?php echo $featured_video->field('vimeo_id'); ?>" width="100%" height="auto" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
   <?php elseif($featured_video->field('video_file_uri')): ?>
-    <source type="video/webm" src="<?php echo $featured_video->field('video_file_uri'); ?>" />
+    <video id="<?php echo $featured_video_uniqid; ?>-player" preload="none" width="320" height="180" style="width: 100%; height: 100%; max-width: 100%;">
+      <source type="video/webm" src="<?php echo $featured_video->field('video_file_uri'); ?>" />
+    </video>
   <?php endif; ?>
-  </video>
 </div>
 <?php endif; // ($featured_video->exists()) ?>

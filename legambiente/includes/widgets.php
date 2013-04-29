bca42fffbcb9d993907893ccabbaf38015c1c0c6
andrea rota <a@xelera.eu>
2013-04-29 11:04:46 +0100

    removed other unused sidebars

--- legambiente/includes/widgets.php
+++ legambiente/includes/widgets.php
@@ -4,6 +4,11 @@ function legambiente_unregister_sidebars() {
   unregister_sidebar( 'home-widget-1' );
 	unregister_sidebar( 'home-widget-2' );
 	unregister_sidebar( 'home-widget-3' );
+  unregister_sidebar( 'right-sidebar' );
+  unregister_sidebar( 'left-sidebar' );
+  unregister_sidebar( 'left-sidebar-half' );
+  unregister_sidebar( 'right-sidebar-half' );
+  unregister_sidebar( 'gallery-widget' );
 }
 
 add_action( 'widgets_init', 'legambiente_unregister_sidebars', 20 );

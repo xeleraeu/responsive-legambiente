09f647faa0033b8d386e71f979b4e62f1ed0fe70
andrea rota <a@xelera.eu>
2013-05-05 17:31:08 +0100

    hook later

--- legambiente/includes/admin-menu-permissions.php
+++ legambiente/includes/admin-menu-permissions.php
@@ -59,6 +59,6 @@ function admin_menu_access_for_editors() {
     var_trace(var_export($submenu, true), 'admin submenu data structure -- after update');
 }
    
-add_action('admin_menu', 'admin_menu_access_for_editors');
+add_action('admin_menu', 'admin_menu_access_for_editors', 1111);
 
 ?>

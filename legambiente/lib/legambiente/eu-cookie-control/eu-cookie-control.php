0114c9bf1409a9bf129587ee82028b5fe4f413ad
andrea rota <a@xelera.eu>
2017-09-11 14:35:54 +0100

    backport old change from production (?WTF?)

--- legambiente/lib/legambiente/eu-cookie-control/eu-cookie-control.php
+++ legambiente/lib/legambiente/eu-cookie-control/eu-cookie-control.php
@@ -48,7 +48,7 @@ class EuCookieControlSettings {
    */
   public $cookie_policy_version;
 
-  function __construct() {
+  function __construct($query_type, $permalink) {
     /**
      * Fetch settings
      */
